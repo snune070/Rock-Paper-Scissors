07703057b2051d7a874863902fbb35d42ec32f87
snune070 <snune070@fiu.edu>
2022-10-30 16:06:28 -0400

    Add game function, added score count to playRound

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
+let computerCount = 0;
+let playerCount = 0;
 
-playRound(getPlayerChoice(), getComputerChoice());
+game();
 
+function game(){
+    console.log("Lets play rock paper scissors");
+    for(i = 0; i < 5; i++){
+        playRound(getPlayerChoice(), getComputerChoice());
+    }
+    if(playerCount > computerCount){
+        console.log(`You won ${playerCount} times, you win`);
+    } else if(playerCount === computerCount){
+        console.log("its a tie");
+    } 
+    else{
+        console.log(`You lost ${computerCount} time, you lose `);
+    }
+}
 
 function playRound(playerInput, computerInput){
 
-    console.log(playerInput);
-    console.log(computerInput);
+
     if(playerInput === "rock" && computerInput === "paper"){
+        computerCount++;
         return console.log("Paper beats rock, you lose");
     } else if(playerInput === "rock" && computerInput === "scissors"){
+        playerCount++;
         return console.log("You WIN!, Rock beats scissors");
     } else if(playerInput === "rock" && computerInput === "rock") {
         return console.log("Its a tie, you both picked rock")
     } else if(playerInput === "paper" && computerInput === "rock") {
+        playerCount++;
         return console.log("You WIN! Paper beats rock");
     } else if(playerInput === "paper" && computerInput === "scissors"){
+        computerCount++;
         return console.log("AW You Lose! Scissors beats Paper");
     } else if(playerInput === "paper" && computerInput === "paper"){
         return console.log("Its a TIE! You both picked paper");
     } else if(playerInput === "scissors" && computerInput === "rock"){
+        computerCount++;
         return console.log("You lose, rock beats scissors");
     } else if(playerInput === "scissors" && computerInput === "paper") {
+        playerCount++;
         return console.log("You Win! Scissors beats Paper");
     } else if(playerInput === "scissors" && computerInput === "scissors") {
         return console.log("Its a tie! you both picked scissors");

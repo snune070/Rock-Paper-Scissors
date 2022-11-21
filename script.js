e3ca3cd4a4339d127494475b38a81b457b722718
snune070 <snune070@fiu.edu>
2022-11-20 22:29:52 -0500

    playRound updates html with correct score count

--- script.js
+++ script.js
@@ -7,35 +7,43 @@ const myButton = document.querySelectorAll('button');
 for (let myButtonElement of myButton) {
     myButtonElement.addEventListener('click', function () {
         let choice = document.getElementById(this.id);
-        myButtonElement = choice.textContent.toLowerCase();
+        myButtonElement = choice.id;
         playRound(myButtonElement, getComputerChoice());
+
     })
 }
 
 function playRound(playerInput, computerInput){
 
+    let playerScore = document.querySelector('.playerScore')
+    let computerScore = document.querySelector('.computerScore')
 
     if(playerInput === "rock" && computerInput === "paper"){
         computerCount++;
-        return console.log("Paper beats rock, you lose");
+        computerScore.innerText = `${computerCount}`;
     } else if(playerInput === "rock" && computerInput === "scissors"){
         playerCount++;
+        playerScore.innerText = `${playerCount}`;
         return console.log("You WIN!, Rock beats scissors");
     } else if(playerInput === "rock" && computerInput === "rock") {
         return console.log("Its a tie, you both picked rock")
     } else if(playerInput === "paper" && computerInput === "rock") {
         playerCount++;
+        playerScore.innerText = `${playerCount}`;
         return console.log("You WIN! Paper beats rock");
     } else if(playerInput === "paper" && computerInput === "scissors"){
         computerCount++;
+        computerScore.innerText = `${computerCount}`;
         return console.log("AW You Lose! Scissors beats Paper");
     } else if(playerInput === "paper" && computerInput === "paper"){
         return console.log("Its a TIE! You both picked paper");
     } else if(playerInput === "scissors" && computerInput === "rock"){
         computerCount++;
+        computerScore.innerText = `${computerCount}`;
         return console.log("You lose, rock beats scissors");
     } else if(playerInput === "scissors" && computerInput === "paper") {
         playerCount++;
+        playerScore.innerText = `${playerCount}`;
         return console.log("You Win! Scissors beats Paper");
     } else if(playerInput === "scissors" && computerInput === "scissors") {
         return console.log("Its a tie! you both picked scissors");
